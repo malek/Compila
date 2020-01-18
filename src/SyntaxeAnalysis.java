cc50f8d8e62e44d69da7d24dcf8b6f14f129454b
lenovo <benzergaamalek@gmail.com>
2020-01-18 17:23:35 +0100

    Add simple syntax analysis

--- src/SyntaxeAnalysis.java
+++ src/SyntaxeAnalysis.java
@@ -1,11 +1,85 @@
-import token.Token;
+import token.*;
 
 import java.util.ArrayList;
 
 public class SyntaxeAnalysis {
-    ArrayList<Token> tkens ;
+    ArrayList<Token> tokens;
+    private int indice;
 
     public SyntaxeAnalysis() {
-        this.tkens = new ArrayList<>();
+        this.tokens = new ArrayList<>();
+        this.indice = 0;
+    }
+
+    public ArrayList<Token> getTokens() {
+        return tokens;
+    }
+
+    public void setTokens(ArrayList<Token> tokens) {
+        this.tokens = tokens;
+        this.indice = 0;
+    }
+
+    public void performSyntaxAnalysis() {
+        if (currentTc().equals(KeywordToken.START_PROGRAM)) {
+            nextTc();
+            instruction();
+            if (currentTc().equals(KeywordToken.END_PROGRAM))
+                System.out.println("goood cooode cooongraraaaaaaaaaaa\n");
+            else System.err.println("bad ending terararara !!!");
+        } else System.err.println("bad starting :D ");
+    }
+
+    private void instruction() {
+        if (currentTc().equals(KeywordToken.GIVE)) {
+            nextTc();
+            give();
+        } else if (currentTc().equals(KeywordToken.SHOW_VAL)) {
+            nextTc();
+            showValeur();
+        } else if (currentTc().equals(KeywordToken.SHOW_MSG)) {
+            nextTc();
+            showMessage();
+        } else {
+            // mot vde
+        }
+    }
+
+    private void showMessage() {
+
+    }
+
+    private void showValeur() {
+
+    }
+
+    private void give() {
+        //System.out.println("goood cooode cooongraraaaaaaaaaaa\n");
+        if (currentTc() instanceof IdToken) {
+            nextTc();
+            if (currentTc().equals(SymbolToken.COLUMN)) {
+                nextTc();
+                if (currentTc() instanceof DataToken) {
+                    nextTc();
+                    if (currentTc().equals(KeywordToken.POINT_VIRGULE)) {
+                        nextTc();
+                        System.out.println("affectation d une valeur a une variable");
+                        return;
+                    }
+                }
+            }
+        }
+
+        System.err.println("error in give instruction");
+    }
+
+    ///////////tc means TOKEN
+    public Token currentTc() {
+
+        return this.tokens.get(indice);
+    }
+
+    public void nextTc() {
+        indice++;
     }
 }
